e4c5b947ef6a12596109ff91f40689eaf343fc64
asifnizar <51851533+asifnizar@users.noreply.github.com>
2019-12-24 15:07:23 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,6 +3,6 @@
  */
 public class Hello {   // Save as "Hello.java" under "d:\myProject"
    public static void main(String[] args) {
-      System.out.println("Hello, world! Asif");
+      System.out.println("Hello, world! Naas123");
    }
 } 
